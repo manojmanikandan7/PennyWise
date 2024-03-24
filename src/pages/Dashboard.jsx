4927d7ce8a27b83fb8b859961031df2e5bd5460a
Max Waring <maxim.waring@student.manchester.ac.uk>
2024-03-24 13:37:31 +0000

    Boilerplate chart using chart.js

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,6 +12,13 @@ import {
 import Navbar from "/src/components/navbar";
 import Sidebar from "/src/components/sidebar";
 
+// Chart.js imports
+import Chart from 'chart.js/auto'
+import { CategoryScale } from "chart.js";
+import LineChart from "/src/components/LineChart";
+
+import { useState } from "react";
+
 //For custom themes
 //TODO: Add the theme colours
 const theme = extendTheme({
@@ -26,7 +33,78 @@ const theme = extendTheme({
   },
 });
 
+// Test Chart.js data
+const testData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234
+  }
+];
+const data = {
+  labels: ['Red', 'Orange', 'Blue'],
+  // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
+  datasets: [
+      {
+        label: 'Popularity of colours',
+        data: [55, 23, 96],
+        // you can set indiviual colors for each bar
+        backgroundColor: [
+          'rgba(255, 255, 255, 0.6)',
+          'rgba(255, 255, 255, 0.6)',
+          'rgba(255, 255, 255, 0.6)'
+        ],
+        borderWidth: 1,
+      }
+  ]
+}
+Chart.register(CategoryScale);
+
 export default function Dashboard() {
+  const [chartData, setChartData] = useState({
+    labels: testData.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: testData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+
   return (
     <ChakraProvider theme={theme}>
       <Grid templateColumns="repeat(7, 1fr)" bg="gray.50">
@@ -103,7 +181,11 @@ export default function Dashboard() {
                 overflow="hidden"
                 p={4}
                 boxShadow="md"
-              ></Box>
+              >
+                <div className="App">
+                  <LineChart chartData={chartData} />
+                </div>
+              </Box>
             </GridItem>
           </SimpleGrid>
         </GridItem>
