48b334b455f8530c494c6251d4b5b37d910827c6
SparkleButt747 <randevpratap@gmail.com>
2024-03-26 20:03:40 +0530

    Updated the back-end on the main branch from the chart-js branch

--- src/back-end/database.js
+++ src/back-end/database.js
@@ -1,28 +1,95 @@
-import mysql from 'mysql2'
+import mysql from "mysql2";
 
-const pool = mysql.createPool({
-    host: 'db4free.net',
-    user: 'pennywise',
-    password: 'password',
-    database: 'pennywise',
-  }).promise()
+const pool = mysql
+  .createPool({
+    host: "db4free.net",
+    user: "pennywise",
+    password: "password",
+    database: "pennywise",
+  })
+  .promise();
 
 export async function createUser(fname, sname, email, password) {
-    const result = await pool.query(`
+  const result = await pool.query(
+    `
     INSERT INTO users (fname, sname, email, password)
     VALUES (?, ?, ?, ?)
-    `, [fname, sname, email, password])
-    return result
+    `,
+    [fname, sname, email, password]
+  );
+  return result;
 }
 
 export async function getLogin(email) {
-    const [rows] = await pool.query(`
+  const [rows] = await pool.query(
+    `
     SELECT email, password FROM users
     WHERE email = ?
-    `, [email])
+    `,
+    [email]
+  );
 
-    if (rows.length == 0) {
-        console.log("No record found for " + email)
+  if (rows.length == 0) {
+    console.log("No record found for " + email);
+  }
+  return rows;
+}
+
+export async function getSpendingData(uid) {
+  const [rows] = await pool.query(
+    `
+    SELECT value, payment_date FROM payments
+    WHERE user_id = ? AND direction = 'out'
+    `,
+    [uid]
+  );
+
+  if (rows.length == 0) {
+    console.log("No payment data found for user with id " + uid);
+  }
+
+  return rows;
+}
+
+export async function getPayments(uid, date) {
+  // Return all the payments from a given day
+
+  var dateObj = new Date(date);
+  var formattedDate =
+    dateObj.getFullYear() +
+    "-" +
+    (dateObj.getMonth() + 1) +
+    "-" +
+    dateObj.getDate();
+
+  const [rows] = await pool.query(
+    `
+    SELECT value, description, category, direction FROM payments
+    WHERE user_id = ? AND payment_date = ?
+    `,
+    [uid, formattedDate]
+  );
+
+  if (rows.length == 0) {
+    console.log(
+      "No payment data found for user with id " +
+        uid +
+        " and date " +
+        formattedDate
+    );
+  }
+
+  for (const row of rows) {
+    row.value = String(row.value);
+
+    // Check if we need to add extra 0's at the end of the value
+    let parts = row.value.split(".");
+    if (parts.length === 1) {
+      row.value += ".00";
+    } else if (parts[1].length === 1) {
+      row.value += "0";
     }
-    return rows
+  }
+
+  return rows;
 }
--- src/back-end/server.js
+++ src/back-end/server.js
@@ -1,52 +1,95 @@
-import express from 'express'
-import bcrypt from 'bcrypt'
-import cors from 'cors'
+import express from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
 
-import { createUser, getLogin } from './database.js'
+import {
+  createUser,
+  getLogin,
+  getPayments,
+  getSpendingData,
+} from "./database.js";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.post("/name", async (req, res) => {
-	const { fname, sname, email, password } = req.body
+  const { fname, sname, email, password } = req.body;
 
-	// Check if the email is already in use
-	const checkUser = await getLogin(email);
-	if (checkUser.length > 0) {
-		res.status(500).send('User with email ' + email + ' already exists.');
-	} else {
-		try {
-			const hash = await bcrypt.hash(password, 15)
-			const name = await createUser(fname, sname, email, hash)
-			res.send(name)
-		} catch (e) {
-			console.error(e)
-			res.status(500).send('An error occurred')
-		}
-	}
-})
+  // Check if the email is already in use
+  const checkUser = await getLogin(email);
+  if (checkUser.length > 0) {
+    res.status(500).send("User with email " + email + " already exists.");
+  } else {
+    try {
+      const hash = await bcrypt.hash(password, 15);
+      const name = await createUser(fname, sname, email, hash);
+      res.send(name);
+    } catch (e) {
+      console.error(e);
+      res.status(500).send("An error occurred");
+    }
+  }
+});
 
 app.post("/login", async (req, res) => {
-	try {
-		const { email, password } = req.body
-		const login = await getLogin(email)
-		const isMatch = await bcrypt.compare(password, login[0].password)
-		if (!isMatch) {
-			res.status(500).send('Invalid login')
-		}
-		res.status(201).send(login)
-	} catch (e) {
-		console.error(e)
-		res.status(500).send('An error occurred')
-	}
-})
+  try {
+    const { email, password } = req.body;
+    const login = await getLogin(email);
+    const isMatch = await bcrypt.compare(password, login[0].password);
+    if (!isMatch) {
+      res.status(500).send("Invalid login");
+    }
+    res.status(201).send(login);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("An error occurred");
+  }
+});
+
+app.get("/dashboard", async (req, res) => {
+  var data = [];
+  const spendingData = await getSpendingData(2).then(function (response) {
+    response.forEach((element) => {
+      // Note: getMonth() returns month from 0-11, hence the +1
+      var date =
+        element.payment_date.getDate() +
+        "/" +
+        (element.payment_date.getMonth() + 1) +
+        "/" +
+        element.payment_date.getFullYear();
+
+      var exists = false;
+      var existingEntry;
+      data.forEach((e) => {
+        if (date === e.x) {
+          exists = true;
+          existingEntry = e;
+        }
+      });
+
+      if (exists) {
+        existingEntry.y += element.value;
+      } else {
+        data.push({ x: date, y: element.value });
+      }
+    });
+  });
+  res.send(data);
+});
+
+app.post("/calendar", async (req, res) => {
+  const { date } = req.body;
+  const data = await getPayments(2, date);
+
+  res.send(data);
+});
 
 app.use((err, req, res, next) => {
-	console.error(err.stack)
-	res.status(500).send('Something broke')
-})
+  console.error(err.stack);
+  res.status(500).send("Something broke");
+});
 
 app.listen(3000, () => {
-	console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
